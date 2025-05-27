60fbbd281fef7a15c8403c748e94233c26b707c6
Md Tarique Azeez <tarique.azeez@technoforte.co.in>
2025-05-27 19:27:19 +0530

    remove unwanted import statement

--- kernel/kernel-idgenerator-service/src/main/java/io/mosip/kernel/uingenerator/repository/UinRepository.java
+++ kernel/kernel-idgenerator-service/src/main/java/io/mosip/kernel/uingenerator/repository/UinRepository.java
@@ -3,8 +3,6 @@ package io.mosip.kernel.uingenerator.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
